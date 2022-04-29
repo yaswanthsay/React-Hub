8a40dab13b224f5a7dcc1dabbd092c09534420c0
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-04-29 16:45:27 +0530

    Goal section edited

--- my-app/src/components/Goal.js
+++ my-app/src/components/Goal.js
@@ -6,7 +6,7 @@ function Goal() {
     const dark = useSelector(state => state.mode.dark)
   return (
     <div>    <div className={dark ? "goal dark" : "goal"}>
-    <h1 className='goal__title'>Our Goals</h1>
+    <h1 className='goal__title'>OUR GOALS</h1>
     <p className='goal__para'>Providing unique websites, interactive apps, and customized marketing plans to expand your business presence.</p>
     </div>
 </div>
