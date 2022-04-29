8a40905f4cb44ece988070e05f2eb942bfaf74fd
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-04-29 16:13:03 +0530

    Cards have been edited

--- my-app/src/productData.js
+++ my-app/src/productData.js
@@ -11,28 +11,28 @@ const productData = [
         "slug":"Redux Toolkit",
         "title":"Redux Toolkit",
         "url":"https://images.unsplash.com/photo-1627398242454-45a1465c2479?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHJlYWN0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-        "description":"Redux Toolkit is one of the popular javascript library"
+        "description":"Redux Toolkit avoids boiler plate codes."
     },
     {
         "id":"3",
         "slug":"Javascript",
         "title":"Javascript",
         "url":"https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8amF2YXNjcmlwdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-        "description":"Javascript is one of the popular javascript library"
+        "description":"Javascript is the language used web."
     },
     {
         "id":"4",
         "slug":"Development",
         "title":"Development",
         "url":"https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNvZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-        "description":"Development is one of the popular javascript library"
+        "description":"We are providing developmental environments"
     },
     {
         "id":"5",
         "slug":"HTML & CSS",
         "title":"HTML & CSS",
         "url":"https://images.unsplash.com/photo-1632882765546-1ee75f53becb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aHRtbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-        "description":"HTML & CSS is one of the popular javascript library"
+        "description":"We could start with HTML and CSS"
     }
 ]
 
--- my-app/src/trendingData.js
+++ my-app/src/trendingData.js
@@ -3,19 +3,19 @@ const trendingData = [
         "id":"1",
         "title":"AI",
         "url":"https://images.unsplash.com/photo-1535378620166-273708d44e4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGFydGlmaWNpYWwlMjBpbnRlbGxpZ2VuY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-        "description":"AI is one of the popular javascript library"
+        "description":"AI  going to create huge wonders"
     },
     {
         "id":"2",
         "title":"ML",
         "url":"https://images.unsplash.com/photo-1591453089816-0fbb971b454c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bWFjaGluZSUyMGxlYXJuaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-        "description":"ML is one of the popular javascript library"
+        "description":"ML will be the most valuable technology in future "
     },
     {
         "id":"3",
         "title":"Cloud Computing",
         "url":"https://media.istockphoto.com/photos/cloud-computing-concept-picture-id1314047007?b=1&k=20&m=1314047007&s=170667a&w=0&h=qlvgtG3oSX9zaYh8KxLgRIJlx3XkXUFxT9OkzI6ADew=",
-        "description":"Cloud Computing is very trending now adays"
+        "description":"Cloud Computing is very trending now a days"
     },
     {
         "id":"4",
