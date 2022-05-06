41bd16c3d8dd1d92ce019edfd236990a60bab375
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-06 18:05:52 +0530

    Instructor page edited

--- my-app/src/components/Instructor.js
+++ my-app/src/components/Instructor.js
@@ -1,23 +1,38 @@
-
 import React from 'react'
+import { useState } from 'react'
+import {NavLink} from 'react-router-dom'
 
 function Instructor() {
+
+  const [instructor,setInstructor] = useState(true) 
+
+
   return (
     <div className='instructor__container'>
       <div className='instructor'>
-      <div className='teacher_img_container'>
-        <img src='/teacher.png' alt='teacher' className='teacher__img'/>
-      </div>
+        <div>
+          {instructor ? <img src='/itteacher1.jpg' alt='' className='itteacher1'/>: ""}
+        </div>
+    {instructor  ? "" :  
+    <div className='instructor_para_container'>
       <div>
-        <img src='/arrow.png' alt='' className='instructor__arrow'/>
-      </div>
-      <div>
-      <h6 className='instructor__title'>BECOME AN INSTRUCTOR</h6>
-      <p className='instructor__para'>Are you passionate to teach your skills? <br/>Then you have an opportunity to become the part of out team.</p>
-      <button className='instructor__btn'>MORE</button>
-      </div>     
+    <p className='instructor__para'>
+    <div className='instructor_para_img'>
+        <img src='/itteacher3.jpg' alt='' className='itteacher3'/>
+      </div><span className='instructor_para_span1'>Why React Hub?
+  
+    <ul className='instructor__unlist'>
+      <li>Long years of experience in IT sector.</li>
+      <li>Free and budget friendly tutorials.</li>
+      <li>Availability of teachers in everytime</li>
+      <li>World class and experienced lectures</li>
+    </ul>
+    </span><br/><span className='instructor_para_span2'>Are you passionate on teaching your skills to the students?Then you are in the right place.</span><br/><NavLink to='/' className='instructor_para_span3' >JOIN WITH US.</NavLink></p>
     </div>
-    </div>
+    </div> }</div> 
+      {instructor ? <img src='/arrow1.png' alt='' className='instructor__arrow'/> : "" }
+      {instructor ? <button className='instructor__btn' onMouseOver={()=>setInstructor(false)} >BECOME AN INSTRUCTOR</button>: "" }
+      </div>  
   )
 }
 
--- my-app/src/components/Premium.js
+++ my-app/src/components/Premium.js
@@ -5,9 +5,7 @@ function Premium() {
   return (
     <div className='premium__container'>
       <div className='premium'>
-        <div>
-          <img src='/arrow.png' alt='' className='premium__arrow'/>
-        </div>
+        
       <div>
       <h6 className='premium__title'>PREMIUM COURSES</h6>
       <p className='premium__para'>Our premium courses are picked up after filteration. <br/>These courses are all taken by experienced instructors.</p>
