9463b3be449554459348df3f555d57c4662f21e3
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-09 17:43:47 +0530

    Instructor page edited

--- my-app/src/components/Instructor.js
+++ my-app/src/components/Instructor.js
@@ -12,6 +12,7 @@ function Instructor() {
          <div className='instructor__img'>
          <img src='/instructor.png' alt='' className='instructor__img'/>
          </div>
+         
          <div className='instructor_btn_container'>
            <p className='instructor__para'>Are you passionate and experienced <br/>in teaching your skills to the students? <br/>Then you are in the right place.You can<br/> join with us to become an instructor</p>
            <button className='instructor__btn' onMouseOver={()=>setWhy(false)}>BECOME AN INSTRUCTOR</button>
--- my-app/src/components/Why.js
+++ my-app/src/components/Why.js
@@ -7,7 +7,7 @@ function Why(props) {
     const [instructorPage,setInstructorPage] = useState(true)
   return (
     <div>
-          <div className='why__popup'>
+         {instructorPage ? <div className='why__popup'>
              <p className='why_popup_para'>Why React Hub?</p>
              <ul>
                <li>More year of experience in IT sector</li>
@@ -15,8 +15,20 @@ function Why(props) {
                <li>Experienced <span onClick={()=>setInstructorPage(false)}>Instructors</span></li>
                <li>Availability of better facilities</li>
              </ul>
-         </div>
-         {instructorPage ? "":<InstructorPage />}
+         </div> : ""}
+         {instructorPage ? "" :<div className='instructor__page'>
+        <h3 className='instructor_page_title'>Who are eligible to become <br/>instructor in our company?</h3>
+        <div className='instructor_page_list'>
+        <ol>
+         <li>Those who have 2 years of experience as an instructor in front end development.</li>
+         <li>Bachelor's degree is a must.</li>
+         <li>Experience as a programmer in an IT company is preferred.</li>
+         <li>Those who have prominent knowledge in backend frameworks has preference</li>
+         <li>Those who are ready to work from office</li>
+        </ol>  
+        <p className='recruiting__contact'>Contact our recruiting team : <span>+91 767747457353</span></p> 
+        </div>  
+    </div>}
     </div>
   )
 }
