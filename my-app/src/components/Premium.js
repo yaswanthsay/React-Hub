6471f752879d43c9d3cbe5580120f2a792c23429
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-10 17:18:00 +0530

    Our works section added and edited

--- my-app/src/components/Premium.js
+++ my-app/src/components/Premium.js
@@ -5,7 +5,7 @@ function Premium() {
   return (
     <div className='premium__container'>
       <div className='premium'>
-        
+    
       <div>
       <h6 className='premium__title'>PREMIUM COURSES</h6>
       <p className='premium__para'>Our premium courses are picked up after filteration. <br/>These courses are all taken by experienced instructors.</p>
