bdcefc777c1d433acb813997a77d3e3013c13bf2
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-12 15:19:37 +0530

    Todo page edited

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -1,30 +1,43 @@
 import React from 'react'
 import { useState } from 'react'
+import Fade from 'react-reveal/Fade';
 
 function Todo() {
- 
-    const [todos,setTodos] = useState()
 
-    const handleSubmit = (e) =>{
-     e.preventDefault();
-     alert(JSON.stringify(e.data))
-    }
+
+    const [toDos, setToDos] = useState()
+    const [toDoLists, setToDoLists] = useState([])
+
 
     const handleChange = (e) => {
-       setTodos(e.target.value)
+        setToDos(e.target.value)
     }
 
+    const handleDelete = () =>{
+        setToDoLists('')
+    }
     return (
         <div className='todo__container'>
             <div className='todo'>
                 <h3 className='todo__title'>React Hub Todo</h3>
-                <form onSubmit={handleSubmit}>
-                <input type='text' name='text' placeholder='New todos...' className='todo__input' onChange={handleChange}/>
-                <button className='todo_add_btn' type='submit'>Add</button>
-                </form>
+                 <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={handleChange} />
+               <Fade bottom>
+                <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, toDos])}>Add</button>
+                </Fade>
                 <div className='todo__space'>
-                    <div className='todo__items'></div>
-                  <p className='todo_items_title'>{todos}</p>
+                        {toDoLists.map((toDoList) => {
+                            return (
+                                <div className='todo__items'>
+                                <input type='checkbox'/>
+                                <div className='todo_title_close'>
+                                <p className='todo_items_title'>{toDoList}</p>
+                                <img src='/pencil.png' alt='' className='todo__pencil' />
+                                <img src='/delete.png' alt='' className='todo__delete' onClick={handleDelete}/>
+                                </div>
+                                </div>
+                            )
+                        })
+                        }
                 </div>
             </div>
         </div>
