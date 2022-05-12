940a8bc6bb1c87c13ecc148e709d0c08c44c8271
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-12 17:15:13 +0530

    Edited

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -13,31 +13,35 @@ function Todo() {
         setToDos(e.target.value)
     }
 
-    const handleDelete = () =>{
+    const handleDelete = () => {
         setToDoLists('')
+    }
+
+    const handleEdit = () => {
+
     }
     return (
         <div className='todo__container'>
             <div className='todo'>
                 <h3 className='todo__title'>React Hub Todo</h3>
-                 <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={handleChange} />
-               <Fade bottom>
-                <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, toDos])}>Add</button>
+                <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={handleChange} />
+                <Fade bottom>
+                    <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, toDos])}>Add</button>
                 </Fade>
                 <div className='todo__space'>
-                        {toDoLists.map((toDoList) => {
-                            return (
-                                <div className='todo__items'>
-                                <input type='checkbox'/>
+                    {toDoLists.map((toDoList) => {
+                        return (
+                            <div className='todo__items'>
+                                <input type='checkbox' />
                                 <div className='todo_title_close'>
-                                <p className='todo_items_title'>{toDoList}</p>
-                                <img src='/pencil.png' alt='' className='todo__pencil' />
-                                <img src='/delete.png' alt='' className='todo__delete' onClick={handleDelete}/>
+                                    <p className='todo_items_title'>{toDoList}</p>
+                                    <img src='/pencil.png' alt='' className='todo__pencil' onClick={handleEdit} />
+                                    <img src='/delete.png' alt='' className='todo__delete' onClick={handleDelete} />
                                 </div>
-                                </div>
-                            )
-                        })
-                        }
+                            </div>
+                        )
+                    })
+                    }
                 </div>
             </div>
         </div>
