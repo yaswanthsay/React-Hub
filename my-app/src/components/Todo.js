38fec8f389c42cf5112c850fc0f3ae6f3dd09f48
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-13 11:28:31 +0530

    Delete functionality added in note app

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -9,34 +9,30 @@ function Todo() {
     const [toDoLists, setToDoLists] = useState([])
 
 
-    const handleChange = (e) => {
-        setToDos(e.target.value)
+    const handleDelete = (event,noteId) => {
+        event.stopPropagation();
+        setToDoLists(prevState => prevState.filter((obj,unknownid)=> unknownid !== noteId)) 
     }
-
-    const handleDelete = () => {
-        setToDoLists('')
-    }
-
-    const handleEdit = () => {
-
+    const handleEdit = (event,noteId) => {
+        event.stopPropagation();
+        setToDos(prevState => prevState.filter)
     }
     return (
         <div className='todo__container'>
             <div className='todo'>
                 <h3 className='todo__title'>React Hub Todo</h3>
-                <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={handleChange} />
+                <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={(e) => setToDos(e.target.value)} />
                 <Fade bottom>
-                    <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, toDos])}>Add</button>
+                    <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, { id: Date.now(), text: toDos, status: false }])}>Add</button>
                 </Fade>
                 <div className='todo__space'>
-                    {toDoLists.map((toDoList) => {
+                    {toDoLists.map((note,id) => {
                         return (
-                            <div className='todo__items'>
-                                <input type='checkbox' />
+                            <div key={note.id} className='todo__items'>                    
                                 <div className='todo_title_close'>
-                                    <p className='todo_items_title'>{toDoList}</p>
-                                    <img src='/pencil.png' alt='' className='todo__pencil' onClick={handleEdit} />
-                                    <img src='/delete.png' alt='' className='todo__delete' onClick={handleDelete} />
+                                    <p className='todo_items_title'>{note.text}</p>
+                                    <img src='/pencil.png' alt='' className='todo__pencil' onClick={(event)=>handleEdit(event,id)} />
+                                    <img src='/delete.png' alt='' className='todo__delete' onClick={(event)=>handleDelete(event,id)} />
                                 </div>
                             </div>
                         )
@@ -49,3 +45,13 @@ function Todo() {
 }
 
 export default Todo
+
+
+    //    <input onChange={(e) => {
+    //                                 setToDoLists(toDoLists.filter(obj2 => {
+    //                                     if (obj2.id === obj.id) {
+    //                                         obj2.status = e.target.value
+    //                                     }
+    //                                     return obj2
+    //                                 }))
+    //                             }} value={obj.status} type='checkbox' /> 
