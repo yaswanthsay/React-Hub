cc6a64a3ab5d9ca6f0a7332f8d52c2456d84ad3e
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-13 15:19:05 +0530

    Todo page edited

--- my-app/src/components/Services.js
+++ my-app/src/components/Services.js
@@ -13,15 +13,15 @@ function Services() {
         <div className='services__list'>    
         <div className='web_service_container'>
             <img src='/web.png' alt='' className='web_service_img'/>
-            <p>Web Development</p>
+            <p className='services__web'>Web Development</p>
         </div>
         <div className='app_service_container'>
         <img src='/app.png' alt='' className='app_service_img'/>
-            <p>App Development</p>
+            <p className='services__app'>App Development</p>
         </div>
         <div className='seo_service_container'>
         <img src='/seo.png' alt='' className='seo_service_img'/>
-            <p>Search Engine Optimisation</p>
+            <p className='services__seo'>Search Engine Optimisation</p>
         </div>
         </div>  
     </div>
