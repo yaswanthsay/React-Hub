4cc3d02cad168f5b2486fdce27e15a2e664a080d
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-13 17:04:03 +0530

    Todo edited

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -21,8 +21,38 @@ function Todo() {
     return (
         <div className='todo__container'>
            
-            
+           <Fade>
+            <div className='todo__left'>
+                <h5>NOTE IT, DO IT</h5>
+                <p className='todo_left_para'>We are including the TO DO page in React Hub.<br/>We are strongly recommending to use To do page daily.<br/>It will help you to complete your task accurately.<br/>We are providing a complete guide for your future.</p>
+                
+            </div>
+            </Fade>
        
+            
+            <Fade >
+                <div className='todo'>
+                    <h3 className='todo__title'>React Hub Todo</h3>
+                    <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={(e) => setToDos(e.target.value)} />
+                    <Fade bottom>
+                        <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, { id: Date.now(), text: toDos, status: false }])}>Add</button>
+                    </Fade>
+                    <div className='todo__space'>
+                        {toDoLists.map((note, id) => {
+                            return (
+                                <div key={note.id} className='todo__items'>
+                                    <div className='todo_title_close'>
+                                        <p className='todo_items_title'>{note.text}</p>
+                                        <img src='/pencil.png' alt='' className='todo__pencil' onClick={(event) => handleEdit(event, id)} />
+                                        <img src='/delete.png' alt='' className='todo__delete' onClick={(event) => handleDelete(event, id)} />
+                                    </div>
+                                </div>
+                            )
+                        })
+                        }
+                    </div>
+                </div>
+            </Fade>
             <div className='todo_img_list'>
             <ul>
                 <li><img src='/pen.png' alt='' className='todo_pen_img' /></li>
@@ -40,36 +70,6 @@ function Todo() {
                 <li><p className='todo__list'>IT'S THE TIME TO WRITE IT DOWN.</p></li>
             </ul>
             </div>
-            <Fade >
-                <div className='todo'>
-                    <h3 className='todo__title'>React Hub Todo</h3>
-                    <input type='text' name='text' placeholder='Type new todos...' className='todo__input' value={toDos} onChange={(e) => setToDos(e.target.value)} />
-                    <Fade bottom>
-                        <button className='todo_add_btn' type='submit' onClick={() => setToDoLists([...toDoLists, { id: Date.now(), text: toDos, status: false }])}>Add</button>
-                    </Fade>
-                    <div className='todo__space'>
-                        {toDoLists.map((note, id) => {
-                            return (
-                                <div key={note.id} className='todo__items'>
-                                    <div className='todo_title_close'>
-                                        <img src='/rightarrow.png' alt='' className='todo__arrow' />
-                                        <p className='todo_items_title'>{note.text}</p>
-                                        <img src='/pencil.png' alt='' className='todo__pencil' onClick={(event) => handleEdit(event, id)} />
-                                        <img src='/delete.png' alt='' className='todo__delete' onClick={(event) => handleDelete(event, id)} />
-                                    </div>
-                                </div>
-                            )
-                        })
-                        }
-                    </div>
-                </div>
-            </Fade>
-            <Fade>
-            <div className='todo__right'>
-                <h5>NOTE IT, DO IT</h5>
-                <p className='todo_right_para'>We are including the TO DO page in React Hub.<br/>We are strongly recommending to use To do page daily.<br/>It will help you to complete your task accurately.</p>
-            </div>
-            </Fade>
         </div>
     )
 }
