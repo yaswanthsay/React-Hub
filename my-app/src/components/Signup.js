f8eafffb690baf1db0b52a654d1a52b56d7573ce
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-18 17:14:37 +0530

    Backend added

--- my-app/src/components/Signup.js
+++ my-app/src/components/Signup.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import {JackInTheBox} from 'react-awesome-reveal'
+import { JackInTheBox } from 'react-awesome-reveal'
+import { useState, useEffect } from 'react'
 
 
 function Signup() {
 
+  const [isSubmit, setIsSubmit] = useState()
+
   const { register, handleSubmit, formState: { errors } } = useForm();
-  
+
+  const onSubmit = (data) => { setIsSubmit(data) }
 
 
+  useEffect(() => {
+    localStorage.setItem("isSubmit", JSON.stringify(isSubmit))
+  }, [isSubmit])
 
- 
-    const onSubmit = (data) =>{JSON.stringify(data)}
+  useEffect(() => {
+    let localSubmit = localStorage.getItem("isSubmit")
+    console.log(localSubmit)
+  }, [isSubmit])
 
 
   return (
@@ -20,19 +29,19 @@ function Signup() {
       <form className='signup__form' onSubmit={handleSubmit(onSubmit)}>
         <h4 className='signup__heading'>REACT HUB</h4>
         <input {...register("username", { required: true, maxLength: 20 })} placeholder='Username' className='username' name='username' type='text' />
-        <div className='username__error'>{errors.username?.type==='required'&& "Username is required"}</div>
+        <div className='username__error'>{errors.username?.type === 'required' && "Username is required"}</div>
         <input {...register("number", { required: true, maxLength: 10 })} placeholder='Number' className='number' name='number' type='number' />
         <div className='number__error'>{errors.number?.type === 'required' && "Number is required"}</div>
         <input {...register("email", { required: true })} placeholder='Email' className='email' name='email' type='email' />
-        <div className='email__error'>{errors.email?.type==='required' && "Email is required"}</div>
+        <div className='email__error'>{errors.email?.type === 'required' && "Email is required"}</div>
         <input {...register("password", { required: true, maxLength: 10, })} placeholder='Password' className='password' name='password' type='password' />
-        <div className='password__error'>{errors.password?.type==='required' && "Password is required"}</div>
+        <div className='password__error'>{errors.password?.type === 'required' && "Password is required"}</div>
         <div>
-        <JackInTheBox>
-        <input className='signup__btn' type='submit' value='SIGNUP'/>
-        </JackInTheBox>
-       <NavLink to='/login' className='already_accnt_link'><p className='already__accnt'>Already have an account?</p></NavLink>
-       </div>
+          <JackInTheBox>
+            <input className='signup__btn' type='submit' value='SIGNUP' />
+          </JackInTheBox>
+          <NavLink to='/login' className='already_accnt_link'><p className='already__accnt'>Already have an account?</p></NavLink>
+        </div>
       </form>
     </div>
   )
