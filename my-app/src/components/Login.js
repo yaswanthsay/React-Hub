bc21299dca0376472ed9d3413375d8c878bd5cc2
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2022-05-19 10:36:42 +0530

    edit product page

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,29 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import {JackInTheBox} from 'react-awesome-reveal'
+import { JackInTheBox } from 'react-awesome-reveal'
 
 
 function Login() {
 
+
+  const [isLogin, setIsLogin] = useState()
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data) =>{alert(JSON.stringify(data))}
+  const onSubmit = (data) => { setIsLogin(data) }
+
+  useEffect(() => {
+    localStorage.setItem("isLogin", JSON.stringify(isLogin))
+  }, [isLogin])
+
+  useEffect(() => {
+    let localLogin = localStorage.getItem("isLogin")
+   alert(localLogin)
+  }, [isLogin])
+
+  
 
 
   return (
     <div className='login'>
       <form className='login__form' onSubmit={handleSubmit(onSubmit)}>
         <h4 className='login__heading'>REACT HUB</h4>
-        <input {...register("email", { required: true})} placeholder='Email' className='email' name='email' type='email' />
+        <input {...register("email", { required: true })} placeholder='Email' className='email' name='email' type='email' />
         <div className='email__error'> {errors.email?.type === 'required' && "Email is required"}</div>
-        <input {...register("password", { required: true, maxLength: 10,minLength: 4 })} placeholder='Password' className='password' name='password' type='password' />
+        <input {...register("password", { required: true, maxLength: 10, minLength: 4 })} placeholder='Password' className='password' name='password' type='password' />
         <div className='password__error'>{errors.password?.type === 'required' && "Password is required"}</div>
         <div className='login__new'>
           <JackInTheBox>
-        <input className='login__btn' type='submit' value='LOGIN'/>
-        </JackInTheBox>
-        {/* <NavLink to='/signup' className='new_account_link'><p className='new__account'>Create new account?</p></NavLink> */}
-      <NavLink to='/signup' className='new_accnt_link'><p className='new__accnt'>Create new account?</p></NavLink>
+            <input className='login__btn' type='submit' value='LOGIN' />
+          </JackInTheBox>
+          {/* <NavLink to='/signup' className='new_account_link'><p className='new__account'>Create new account?</p></NavLink> */}
+          <NavLink to='/signup' className='new_accnt_link'><p className='new__accnt'>Create new account?</p></NavLink>
         </div>
       </form>
     </div>
--- my-app/src/components/Product.js
+++ my-app/src/components/Product.js
@@ -1,49 +1,18 @@
-// import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
-// import { Link } from 'react-router-dom'
 
-
-// export default function MediaCard(props) {
-
-
-//   return (
-// <div className='product__container' id='productId'>
-//     <Card sx={{ maxWidth: 345, maxHeight: 360 }} className='product' >
-//       <Link to={`/productpage/${props.slug}`}>
-//         <CardMedia
-//           className='product__img'
-//           component="img"
-//           height="130"
-//           image={props.url}
-//           alt={props.title}
-//         />
-//       </Link>
-//       <CardContent>
-//         <Typography variant="h6" component="div">
-//           {props.title}
-//         </Typography>
-//       </CardContent>
-//     </Card>
-//     </div>
-//   );
-// }
 import React from 'react'
 import Fade from 'react-reveal/Fade';
 import { NavLink } from 'react-router-dom';
 
 function Product(props) {
   return (
-    <NavLink to={`/productpage/${props.slug}`} className='product__link'>
-    <div className='product'>
-      <Fade bottom>
-      <img src={props.url} alt='' className='product__img'/>
-      </Fade>
-      <p className='product__para'>{props.description}</p>
-      <h4 className='product__title'>{props.title}</h4>
-    </div>
+    <NavLink to={`/productpage/${props.description}`} className='product__link'>
+      <div className='product'>
+        <Fade bottom>
+          <img src={props.url} alt='' className='product__img' />
+        </Fade>
+        <p className='product__para'>{props.description}</p>
+        <h4 className='product__title'>{props.title}</h4>
+      </div>
     </NavLink>
   )
 }
